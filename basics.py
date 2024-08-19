3c4d06cf81f7d6366521645b57c391ddf9dd04e7
ТÐsafelanman <safelanman@gmail.com>
2024-08-19 05:21:29 +0300

    Добавление .gitignore

---
+++ basics.py
@@ -0,0 +1,16 @@
+name = "Alice"
+age = 30
+print(f"Name: {name}, Age: {age}")
+print()	# Пустая строка
+
+number = -5
+if number > 0:
+	print("Число положительное\n")
+elif number == 0:
+	print("Число равно нулю\n")
+else:
+	print("Число отрицательное\n")
+
+for i in range(1, 6):
+	print(i)
+
